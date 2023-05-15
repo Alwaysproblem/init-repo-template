9a2389b7298a29d6328b3372275f2d28f85a36bb
Alwaysproblem <reganyang0415@gmail.com>
2023-05-15 13:43:03 +0800

    support conda env

---
+++ .vscode/.envsetup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source $HOME/opt/anaconda3/etc/profile.d/conda.sh && conda activate cpp-simplecode
+# source /root/anaconda3/etc/profile.d/conda.sh && conda activate <env_name>
+# source /root/micromamba/etc/profile.d/micromamba.sh && micromamba activate <env_name>
