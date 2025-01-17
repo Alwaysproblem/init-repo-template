9538d1f85575090df773b4321cd36a9deb3701a2
Alwaysproblem <reganyang0415@gmail.com>
2025-01-17 11:12:38 +0800

    Add conda env for cmake configuration

---
+++ .vscode/.envsetup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source $HOME/anaconda3/etc/profile.d/conda.sh && conda activate cpp-simplecode
+# source /root/anaconda3/etc/profile.d/conda.sh && conda activate <env_name>
+# source /root/micromamba/etc/profile.d/micromamba.sh && micromamba activate <env_name>
